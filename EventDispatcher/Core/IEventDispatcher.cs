b7cfb23e93bf91e4aa4c2ffa9c93a64ff00af9ca
ggdvalishvili@gmail.com <ggdvalishvili@gmail.com>
2017-03-20 00:04:40 +0400

    add event dispatcher

---
+++ EventDispatcher/Core/IEventDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace EventDispatcher.Core
+{
+    public interface IEventDispatcher<TDomainEvent>
+    {
+        void RegisterHandlers(Assembly assembly);
+        void Register<TEvent, THandler>();
+        void Dispatch(dynamic evnt);
+        void Dispatch(List<TDomainEvent> domainEvents);
+    }
+}
